02303763c1f18dbdbf7c7d5f2799e02ba4f599cf
AHaliq <AHaliq@users.noreply.github.com>
2021-11-12 06:42:55 +0000

    deploy: 0f600c2ba48571f6b802f17ae9b1668d610d145c

--- tmp/source/simple.nix
+++
@@ -1,9 +0,0 @@
-{ system ? builtins.currentSystem
-}:
-let 
-  name = "reflex-stone";
-  p = import ./project.nix { inherit system; };
-  pkgs = p.reflexPlatform.nixpkgs;
-  app = pkgs.lib.getAttr name p.project.ghcjs;
-in
- app
