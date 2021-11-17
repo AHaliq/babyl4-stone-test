a3691a2839fa5c8841b1cccdfcc63c57cb608107
Andreas Källberg <anka.213@gmail.com>
2021-11-17 13:45:44 +0800

    Remove ffi based code and only use jsaddle

--- frontend/src/Widgets/Page/TwoWindow/OutputWindow/JSEcho.hs
+++ frontend/src/Widgets/Page/TwoWindow/OutputWindow/JSEcho.hs
@@ -8,21 +8,11 @@ where
 import qualified Data.Text as T
 import Reflex.Dom.Core
 
-#ifdef MIN_VERSION_ghcjs_dom_jsffi
-import Control.Monad.Trans (liftIO)
-import qualified GHCJS.DOM.Types as GDT (pToJSVal)
-import GHCJS.Types (JSVal)
-
-foreign import javascript safe "console.log $1"
-  jslog :: JSVal -> IO ()
-
-widget :: forall t m. MonadWidget t m => Dynamic t T.Text -> m ()
-widget l4ast =
-  performEvent_ $ fmap liftIO $ updated $ jslog . GDT.pToJSVal . T.unpack <$> l4ast
-
-#else
 import Language.Javascript.JSaddle
 import Control.Monad (void)
+
+-- foreign import javascript safe "console.log $1"
+--   jslog :: JSVal -> IO ()
 
 jsaddlePrint :: MonadJSM m => String -> m ()
 -- jsaddlePrint s = void $ liftJSM $ eval $ "console.log(" <> show s <> ")"
@@ -32,4 +22,3 @@ widget :: forall t m. MonadWidget t m => Dynamic t T.Text -> m ()
 -- widget _ = return ()
 widget l4ast =
   performEvent_ $ updated $ jsaddlePrint . T.unpack <$> l4ast
-#endif
